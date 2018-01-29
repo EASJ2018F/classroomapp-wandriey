3c51ed2c75f8364338e5db1654f968d1e1e7225c
wandriey <jona434g@edu.easj.dk>
2018-01-29 18:22:21 +0100

    Studerende class oprettet
    3 instansfields og til svarende propperties, samt en constructor der tager 3 parametre.

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Studerende
+    {
+        public string _navn;
+        public int _fødselsmåned;
+        public int _fødselsdag;
+
+        public Studerende(string Navn, int fødselsmåned, int fødselsdag)
+        {
+            _navn = Navn;
+            _fødselsmåned = fødselsmåned;
+            _fødselsdag = fødselsdag;
+        }
+
+        public string Navn{ get { return _navn; } set { _navn = value; } }
+        public int Fødselsmåned { get { return _fødselsmåned; } set { _fødselsmåned = value; } }
+        public int Fødselsdag { get { return _fødselsdag; } set { _fødselsdag = value; } }
+
+    }
+}
