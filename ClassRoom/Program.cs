3865fc102f764a8c1dad32e2ff3dc4a2f09bfe2f
wandriey <jona434g@edu.easj.dk>
2018-01-29 19:55:31 +0100

    Metode i studerende "årstid"
    Kan finde ud af hvilken årstid du er født i på baggrund af din fødselsmåned

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -16,18 +16,30 @@ namespace ClassRoom
 
             Console.WriteLine($"Semester start er: {klasseRumA.SemesterStart}");   //Checking the data is set. 
 
-            klasseRumA.Klasseliste.Add(new Studerende("Jonas", 06, 22));
-            klasseRumA.Klasseliste.Add(new Studerende("Ian", 06, 23));
-            klasseRumA.Klasseliste.Add(new Studerende("Kasper", 08, 05));
-            klasseRumA.Klasseliste.Add(new Studerende("Christoffer", 02, 3));
+            Studerende Jonas = new Studerende("Jonas", 06, 22);
+            Studerende Ian = new Studerende("Ian", 06, 23);
+            Studerende Kasper = new Studerende("Kasper", 08, 05);
+            Studerende Christoffer = new Studerende("Christoffer", 02, 3);
+            klasseRumA.Klasseliste.Add(Jonas);
+            klasseRumA.Klasseliste.Add(Ian);
+            klasseRumA.Klasseliste.Add(Kasper);
+            klasseRumA.Klasseliste.Add(Christoffer);
 
-            foreach (var item in klasseRumA.Klasseliste)
+            //klasseRumA.Klasseliste.Add(new Studerende("Jonas", 06, 22));             //instead og first create and then add
+            //klasseRumA.Klasseliste.Add(new Studerende("Ian", 06, 23));          <--- //you can do it in one go 
+            //klasseRumA.Klasseliste.Add(new Studerende("Kasper", 08, 05));            
+            //klasseRumA.Klasseliste.Add(new Studerende("Christoffer", 02, 3));
+
+            foreach (var Student in klasseRumA.Klasseliste)
             {
-                Console.WriteLine(item);                          //making sure our students are properly created 
+                Console.WriteLine($"{Student.Navn} født d.{Student.Fødselsdag}/{Student.Fødselsmåned} i årstiden {Student.Årstid(Student.Fødselsmåned)} ");                     
             }
 
+            //Console.WriteLine($"Jonas har født i årstiden: {Jonas.Årstid(Jonas.Fødselsmåned)}");   //you can also check it one by one like this
+
+
+
             Console.ReadLine();
-         
         }
     }
 }
--- ClassRoom/Studerende.cs
+++ ClassRoom/Studerende.cs
@@ -24,6 +24,27 @@ namespace ClassRoom
         public int Fødselsdag { get { return _fødselsdag; } set { _fødselsdag = value; } }
 
 
+        public string Årstid(int fødselsmåned)                                        //the reason why the metod is in here, is becouse we easyly can 
+        {                                                                             //get the _fødselsmåned.
+            if (fødselsmåned == 12 || fødselsmåned == 1 || fødselsmåned == 2)         //and also becouse its some logik that is specifik for a 
+            {                                                                         //student
+               return ("Vinter");
+            }
+            if (fødselsmåned == 3 || fødselsmåned == 4 || fødselsmåned == 5)
+            {
+                return ("forår");
+            }
+            if (fødselsmåned == 6 || fødselsmåned == 7 || fødselsmåned == 8)
+            {
+                return ("sommer");
+            }
+            if (fødselsmåned == 9 || fødselsmåned == 10 || fødselsmåned == 11)
+            {
+                return ("Efterår");
+            }
+            return "Måneden eksitere ikke";
+        }
+
 
         public override string ToString()
         {
