66879fe3803f2692bf3c111b11b16e49e5ab2c20
wandriey <jona434g@edu.easj.dk>
2018-01-29 18:28:10 +0100

    KlasseRum class oprettet
    3 instansfields (klassenavn, klasselist og semesterstart) + tilsvarende propperties.
    inkl default contructor.

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        public string _klasseNavn;
+        public List<Studerende> _klasseliste;
+        public DateTime _semesterStart;
+
+        public KlasseRum()
+        {
+                
+        }
+
+        public string _KasseNavn { get { return  _KasseNavn; } set { _KasseNavn = value; } }
+        public List<Studerende> Klasseliste { get { return _klasseliste; } set { _klasseliste= value; } }
+        public DateTime SemesterStart { get { return _semesterStart; } set { _semesterStart = value; } }
+    }
+}
